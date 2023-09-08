dfe9e4041d423f0cdf0ded45dcce66400e7ceb99
kanailalmanna <kanailalmanna2003@gmail.com>
2023-09-08 15:26:27 +0530

    Good luck

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,14 +16,17 @@ import { Context } from '../context/contextApi';
 import Loader from '../shared/Loader';
 
 const Header = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { loading, mobileMenu, setMobileMenu } = useContext(Context);
 
   const navigate = useNavigate();
 
   const searchQueryHandler = (event) => {
-    if ((event.key === 'Enter' || event === 'searchButton') && searchQuery.length > 0) {
+    if (
+      (event?.key === "Enter" || event === "searchButton") &&
+      searchQuery?.length > 0
+    ) {
       navigate(`/searchResult/${searchQuery}`);
     }
   };
@@ -33,32 +36,39 @@ const Header = () => {
   };
 
   const { pathname } = useLocation();
-  const pageName = pathname.split('/').filter(Boolean)[0];
+  const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
 
+  
   return (
     <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
       {loading && <Loader />}
 
       <div className="flex h-5 items-center">
-        {pageName !== 'video' && (
+        {pageName !== "video" && (
           <div
             className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
             onClick={mobileMenuToggle}
           >
             {mobileMenu ? (
-              <CgClose className="text-white text-xl" /> // Use CgClose for close
+              <CgClose className="text-white text-xl" />
             ) : (
-              <CgMenu className="text-white text-xl" /> // Use CgMenu for open
+              <SlMenu className="text-white text-xl" />
             )}
           </div>
         )}
-
         <Link to="/" className="flex h-5 items-center">
-          <img className="h-full hidden md:block" src={ytLogo} alt="Vidtube" />
-          <img className="h-full hidden md:block" src={ytLogoMobile} alt="Vidtube" />
+          <img
+            className="h-full hidden dark:md:block"
+            src={ytLogo}
+            alt="Youtube"
+          />
+          <img
+            className="h-full md:hidden"
+            src={ytLogoMobile}
+            alt="Youtube"
+          />
         </Link>
       </div>
-
       <div className="group flex items-center">
         <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0">
           <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
@@ -80,7 +90,6 @@ const Header = () => {
           <IoIosSearch className="text-white text-xl" />
         </button>
       </div>
-
       <div className="flex items-center">
         <div className="hidden md:flex">
           <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
@@ -94,9 +103,9 @@ const Header = () => {
           <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" />
         </div>
       </div>
-
     </div>
   );
 };
+
 
 export default Header;
