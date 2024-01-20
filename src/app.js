912ed7d185e76a8a8e0bb88537ad1a7c9c17061d
Kanailal Manna <kanailalmanna2003@gmail.com>
2024-01-21 00:10:41 +0530

    Health Check 🧑🏻‍⚕️

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import tweetRouter from "./routes/tweets.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
+import healthcheckRouter  from "./routes/healthCheck.routes.js";
 
 
 //routes declaration
@@ -34,6 +35,7 @@ app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 
 
